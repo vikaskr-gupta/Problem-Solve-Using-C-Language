b3f7c8f9000b9ee880a5a2c989b97273381c0a80
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-06-11 15:21:58 +0530

    Create 4_gets_puts_uses.c

---
+++ Lab-Assignments/Basic Input Output Operations/4_gets_puts_uses.c
@@ -0,0 +1,12 @@
+// Write program using string input and output function ( gets and puts)
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name[30];
+    printf("Enter a String :\n");
+    gets(name);
+    puts(name);
+    getch();
+}
