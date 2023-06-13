2bf7367a6554e62fa6ec8e6a599d9fa7220a4a04
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-06-14 02:48:22 +0530

    Create 2_Using_for_loop_print_10_to_1.c

---
+++ Problem solving/Loop/2_Using_for_loop_print_10_to_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    printf("Printing 1 to 10 numbers in Reverse order :\n");
+    for(i = 10; i >= 1; i--)
+    {
+        printf("%d\n", i);
+    }
+    getch();
+}
