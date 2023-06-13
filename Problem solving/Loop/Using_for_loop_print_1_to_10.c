ab4eef52fedce5b3715076dddf1228127ac19e61
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-06-14 02:47:06 +0530

    Create Using_for_loop_print_1_to_10.c

---
+++ Problem solving/Loop/Using_for_loop_print_1_to_10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    printf("Printing 1 to 10 numbers :\n");
+    for(i=0;i<=10;i++)
+    {
+        printf("%d\n", i);
+    }
+    getch();
+}
