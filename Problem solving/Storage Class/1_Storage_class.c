85a36859c43704a2a0c6a3c42e463d36a5e383cd
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-06-29 20:20:45 +0530

    Create 1_Storage_class.c

---
+++ Problem solving/Storage Class/1_Storage_class.c
@@ -0,0 +1,14 @@
+// Data Type Declaration Instruction
+// 1.Name:
+// 2.Size:
+// 3.Data Type:
+// 4.Default Value:
+// 5.Storage:
+// 6.Life:
+// 7.Scope:
+
+// storage_class  Data_type  Name
+// autimatic      int        x = 5;
+// register       char
+// static         float
+// external       double
