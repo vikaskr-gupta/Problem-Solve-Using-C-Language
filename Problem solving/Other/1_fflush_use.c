71dd99bfcac316b9a91baa0b29dc3a6455bc80cc
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-07-02 00:06:10 +0530

    Create 1_fflush_use.c

---
+++ Problem solving/Other/1_fflush_use.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char x, y;
+    printf("Enter a Character: ");
+    scanf("%c",&x);
+    fflush(stdin);
+    printf("Enter a Second Character: ");
+    scanf("%c",&y);
+    getch();
+}
